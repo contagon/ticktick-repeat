112ea11f2003bd6c3ff3bb4b2ea606c0f5137184
Easton <contagon6@gmail.com>
2020-05-26 22:43:28 -0600

    Logic all in, just need to put in Notion integration

--- startup.py
+++ startup.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, flash, redirect, url_for, request
+from flask import Flask, render_template, flash, redirect, url_for, request, session
 from flask_wtf import FlaskForm
 from wtforms import Form, SubmitField, StringField
 from wtforms.validators import DataRequired, URL
 
 import os
 
-DEBUG = False
+# session['connected'] = False
 
 class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
@@ -13,7 +13,7 @@ class Config():
 class NotionLocation(FlaskForm):
     link = StringField('URL', validators=[DataRequired(), URL()])
     token = StringField('v2 Token')
-    submit = SubmitField('Get Access')
+    submit = SubmitField('Submit')
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -21,19 +21,45 @@ app.config.from_object(Config)
 @app.route("/", methods=['GET', 'POST'])
 def home():
     form = NotionLocation()
-    if form.validate_on_submit():
-        flash("Checking for connection...")
-        return redirect(url_for("submit", form=form))
-    # Debugging
-    elif DEBUG:
-        flash(form.errors)
+
+    # If they put in good parameters and haven't connected yet
+    if form.validate_on_submit() and session['connected'] == False:
+        #see if what they gave us works
+        try:
+            ##TODO:  try to connect here
+            session['connected'] = True
+        except:
+            #if we fail to connect, flash message and render OG form
+            flash("Input Parameters don't seem to work!")
+            return render_template('home.html', form=form)
+
+        #if we connected properly
+        ##TODO:  pull columns out of notions to fill full form
+        class FullForm(NotionLocation):
+            column = StringField('Some Column')
+        new_form = FullForm()
+        return render_template('home.html', form=new_form)
+
+    # If they put in good parameters and have already connected
+    if form.validate_on_submit() and session['connected'] == True:
+        ##TODO:  pull columns out of notions to fill full form
+        class FullForm(NotionLocation):
+            column = StringField('Some Column')
+        new_form = FullForm()
+
+        ##TODO:  push it to notion!
+        flash(f"INSERTED: {new_form.column.data}")
+
+        #empty slots we don't want to reuse
+        for field in new_form:
+            if field.name not in ["link", "token"] and field.widget.input_type != 'hidden':
+                field.data = ""
+
+        return render_template('home.html', form=new_form)
+
+    #Just starting or bad input
+    session['connected'] = False
     return render_template('home.html', form=form)
-
-@app.route("/submit")
-def submit():
-    form = request.args['form']
-    return type(form)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
