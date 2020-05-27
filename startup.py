37644965bb05985688026cb292ab71aeb747f403
Easton <contagon6@gmail.com>
2020-05-27 14:15:16 -0600

    Got new form set up. Need to expand form making and iteratively adding things

--- startup.py
+++ startup.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, flash, redirect, url_for, request, session
 from flask_wtf import FlaskForm
-from wtforms import Form, SubmitField, StringField
+from wtforms import SubmitField, StringField, BooleanField, DateTimeField, SelectField
 from wtforms.validators import DataRequired, URL
+from notion.client import NotionClient
 
 import os
 
@@ -11,12 +12,42 @@ class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
 class NotionLocation(FlaskForm):
-    link = StringField('URL', validators=[DataRequired(), URL()])
+    url = StringField('URL', validators=[DataRequired(), URL()])
     token = StringField('v2 Token')
     submit = SubmitField('Submit')
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+def connect_notion(url, token):
+    if token == "":
+        token = "BOT_TOKEN"
+    client = NotionClient(token_v2=token)
+    view = client.get_collection_view(url)
+    return view.collection
+
+def make_full_form(schema):
+    #Put all columns into  a field
+    fields = dict()
+    for column in schema:
+        if column['type'] == 'title' or column['type'] == 'text':
+            fields[ column['slug'] ] = StringField(column['name'], validators=[])
+        elif column['type'] == 'checkbox':
+            fields[ column['slug'] ] = BooleanField(column['name'], validators=[])
+        elif column['type'] == 'date':
+            fields[ column['slug'] ] = DateTimeField(column['name'], validators=[])
+        elif column['type'] == 'select':
+            fields[ column['slug'] ] = SelectField(column['name'], validators=[], choices=[("test", "test"), ("hi", "hi")])
+        # elif column['type'] == 'multi_select':
+        #     fields[ column['slug'] ] = BooleanField(column['name'], validators=[])
+    #make temporary class to hold our form
+    class FullForm(NotionLocation):
+        pass
+    #apply all attributes of our temporary form
+    for k, v in fields.items():
+        setattr(FullForm, k, v)
+
+    return FullForm()
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
@@ -26,36 +57,35 @@ def home():
     if form.validate_on_submit() and session['connected'] == False:
         #see if what they gave us works
         try:
-            ##TODO:  try to connect here
+            collection = connect_notion(form.url.data, form.token.data)
             session['connected'] = True
-        except:
+        except Exception as e:
             #if we fail to connect, flash message and render OG form
-            flash("Input Parameters don't seem to work!")
+            flash(e)
             return render_template('home.html', form=form)
 
         #if we connected properly
-        ##TODO:  pull columns out of notions to fill full form
-        class FullForm(NotionLocation):
-            column = StringField('Some Column')
-        new_form = FullForm()
-        return render_template('home.html', form=new_form)
+        full_form = make_full_form(collection.get_schema_properties())
+        return render_template('home.html', form=full_form)
 
     # If they put in good parameters and have already connected
-    if form.validate_on_submit() and session['connected'] == True:
-        ##TODO:  pull columns out of notions to fill full form
-        class FullForm(NotionLocation):
-            column = StringField('Some Column')
-        new_form = FullForm()
+    if session.get('connected') == True:
+        #pull columns out of notions to fill full form
+        collection = connect_notion(form.url.data, form.token.data)
+        full_form = make_full_form(collection.get_schema_properties())
 
-        ##TODO:  push it to notion!
-        flash(f"INSERTED: {new_form.column.data}")
+        if form.validate_on_submit(): 
+            ##TODO:  push it to notion!
+            flash(f"INSERTED: {full_form.data}")
 
-        #empty slots we don't want to reuse
-        for field in new_form:
-            if field.name not in ["link", "token"] and field.widget.input_type != 'hidden':
-                field.data = ""
+            #empty slots we don't want to reuse
+            for field in full_form:
+                if field.name not in ["url", "token"]:# and field.widget.input_type != 'hidden':
+                    field.data = ""
 
-        return render_template('home.html', form=new_form)
+            return render_template('home.html', form=full_form)
+
+        return render_template('home.html', form=full_form)
 
     #Just starting or bad input
     session['connected'] = False
